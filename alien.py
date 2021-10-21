bd0f842f43025f2507989600fc8446d44cf9471f
ZJ <zhaozj1225@outlook.com>
2021-10-21 15:04:12 +0800

    完成章节13.2.1

---
+++ alien.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """A class to express an alien."""
+
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+
+        # load alien image and set rect
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        # set the position near the lefttop of screen
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # store the accurate x position
+        self.x = float(self.rect.x)
