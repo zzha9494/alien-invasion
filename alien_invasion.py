bbbcdf693a07cc173d10420599c228435c7a1e50
ZJ <zhaozj1225@outlook.com>
2021-10-24 22:26:30 +0800

    完成章节13.7

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,9 +35,12 @@ class AlienInvasion:
         """start the main loop."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
     def _check_events(self):
