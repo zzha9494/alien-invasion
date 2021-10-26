c35f3d6ebda0a4284cf2ebba255e4fc65681f232
ZJ <zhaozj1225@outlook.com>
2021-10-26 22:27:38 +0800

    完成章节14.1.3

--- alien_invasion.py
+++ alien_invasion.py
@@ -56,6 +56,14 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """start game when click play button"""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
 
     def _check_keydown_events(self, event):
         """response keydown events"""
